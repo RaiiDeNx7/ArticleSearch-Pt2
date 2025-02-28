1da1adfdb87bf746dbb37677cc21d583005a36ee
Hunter Smith <huntertsmith4778@gmail.com>
2025-02-27 20:22:02 -0500

    Dependencies updated

--- app/src/main/java/com/codepath/articlesearch/Article.kt
+++ app/src/main/java/com/codepath/articlesearch/Article.kt
@@ -2,6 +2,7 @@ package com.codepath.articlesearch
 import android.support.annotation.Keep
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+//Test
 
 @Keep
 @Serializable
